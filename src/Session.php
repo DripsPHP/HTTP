383fa7e52fc601ef0fb0cd1474fcee2268d26d08
Raffael Kessler <raffael.kessler@prowect.com>
2016-01-29 15:57:06 +0100

    Session integriert

---
+++ src/Session.php
@@ -0,0 +1,204 @@
+<?php
+
+/**
+ * Created by Prowect
+ * Author: Raffael Kessler
+ * Date: 02.11.2015 - 10:00
+ * Copyright: Prowect
+ */
+
+namespace Drips\HTTP;
+
+use Drips\DataStructures\IDataCollection;
+
+class Session implements IDataCollection
+{
+    /**
+     * Beinhaltet eine ID unter der die Session-Daten gespeichert werden
+     *
+     * @var string
+     */
+    private $id;
+
+    /**
+     * Beinhaltet eine ID unter der id Informationen zu den Session-Daten
+     * gespeichert werden
+     *
+     * @var string
+     */
+    private $id_info;
+
+    /**
+     * Erzeugt eine neue Session-Instanz.
+     * Es sollte bei jedem Seitenaufruf ein solches Session-Objekt angelegt
+     * werden, damit die Session-Informationen auch entsprechend wieder entfernt
+     * werden können.
+     *
+     * @param string $id Die ID unter der die Session-Daten gespeichert werden sollen
+     */
+    public function __construct($id = "DRIPS")
+    {
+        session_name($id);
+        $this->id = $id;
+        $this->id_info = $this->id."_INFO";
+        if(session_status() != PHP_SESSION_ACTIVE){
+            session_start();
+        }
+        $this->cleanup();
+    }
+
+    /**
+     * Dient zum "Aufräumen" der Session-Information. Löscht veraltete oder
+     * abgelaufene Session-Informationen.
+     */
+    private function cleanup()
+    {
+        if(isset($_SESSION[$this->id_info])){
+            foreach($_SESSION[$this->id_info] as $key => $val){
+                $expire = $this->getSessionInfo($key, "expire");
+                if($expire){
+                    $this->setSessionInfo($key, "expire", -1);
+                } elseif($expire == -1) {
+                    unset($_SESSION[$this->id_info][$key]);
+                    unset($_SESSION[$this->id][$key]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Führt beim Beenden des Scripts bzw. beim "Beenden" der Session ein cleanup
+     * durch.
+     */
+    public function __destruct()
+    {
+        $this->cleanup();
+    }
+
+    /**
+     * Überprüft ob in der Session ein Wert unter dem Schlüssel $key hinterlegt
+     * ist.
+     *
+     * @param string $key Schlüssel nach dem gesucht werden soll
+     *
+     * @return bool
+     */
+    public function has($key)
+    {
+        return isset($_SESSION[$this->id][$key]);
+    }
+
+    /**
+     * Gibt den Wert der Session eines zugehörigen Schlüssels zurück.
+     * Sofern der Schlüssel nicht existiert wird NULL zurückgeliefert.
+     *
+     * @param string $key Schlüssel nach dem gesucht werden soll
+     *
+     * @return mixed
+     */
+    public function get($key)
+    {
+        if($this->has($key)){
+            $val = $this->getSession($key);
+            $unserialized = @unserialize($val);
+            if($unserialized !== false){
+                return $unserialized;
+            }
+            return $val;
+        }
+        return null;
+    }
+
+    /**
+     * Liefert alle Session-Daten dieses Objektes zurück.
+     *
+     * @return mixed
+     */
+    public function getAll()
+    {
+        if(isset($_SESSION[$this->id])){
+            return $_SESSION[$this->id];
+        }
+        return array();
+    }
+
+    /**
+     * Setzt einen Wert in der Session.
+     * Wird $expire gesetzt, so hat die Information nur zeitlich beschränkte
+     * Gültigkeit.
+     *
+     * @param string $key Der Schlüssel hinter dem die Information hinterlegt werden soll
+     * @param mixed $val Der Wert der in der Session gespeichert werden soll
+     * @param mixed $expire Gibt an ob die Information ablaufen soll
+     */
+    public function set($key, $val, $expire = null)
+    {
+        if(is_object($val)){
+            $val = serialize($val);
+        }
+        $this->setSession($key, $val);
+        if($expire != null){
+            $this->setSessionInfo($key, "expire", true);
+        }
+    }
+
+    /**
+     * Zuständig für den direkten Zugriff auf die Session.
+     * Setzt einen Wert für einen zugehörigen Schlüssel.
+     *
+     * @param string $key Der Schlüssel hinter dem die Information hinterlegt werden soll
+     * @param mixed $val Der Wert der in der Session gespeichert werden soll
+     */
+    private function setSession($key, $val)
+    {
+        $_SESSION[$this->id][$key] = $val;
+    }
+
+    /**
+     * Dient zum Setzen von Zusatzinformationen für die Session-Daten.
+     *
+     * @param string $key Der Schlüssel für die dazugehörige Session-Information
+     * @param string $name Der Name der Zusatzinformation
+     * @param mixed $val Der Wert der Zusatzinformation
+     */
+    private function setSessionInfo($key, $name, $val)
+    {
+        if($this->has($key)){
+            $_SESSION[$this->id_info][$key][$name] = $val;
+        }
+    }
+
+    /**
+     * Dient zum direkten Zugriff auf die Session und liefert den Wert eines
+     * dazugehörigen Schlüssels.
+     *
+     * @param string $key Der Schlüssel hinter dem die Session-Information hinterlegt ist
+     *
+     * @return mixed
+     */
+    private function getSession($key)
+    {
+        return isset($_SESSION[$this->id][$key]) ? $_SESSION[$this->id][$key] : null;
+    }
+
+    /**
+     * Dient zum Auslesen der Zusatzinformationen der Session-Daten.
+     * Wird kein Name angegeben so werden alle Zusatzinformationen der Session-
+     * Daten zurückgeliefert.
+     *
+     * @param string $key Der Schlüssel für die dazugehörige Session-Information
+     * @param string $name Der Name der Zusatzinformation
+     *
+     * @return mixed
+     */
+    private function getSessionInfo($key, $name = null)
+    {
+        if($this->has($key)){
+            if($name == null){
+                return $_SESSION[$this->id_info][$key];
+            }
+            return $_SESSION[$this->id_info][$key][$name];
+        }
+        return null;
+    }
+}
---
+++ tests/TestSession.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace tests;
+
+session_start();
+require_once __DIR__.'/../vendor/autoload.php';
+
+use PHPUnit_Framework_TestCase;
+use Drips\HTTP\Session;
+use StdClass;
+
+class TestSession extends PHPUnit_Framework_TestCase
+{
+    public function testSession()
+    {
+        $session = new Session();
+        $session->set("one", 1);
+        $session->set("two", 2);
+        $this->assertTrue($session->has("one"));
+        $this->assertFalse($session->has("three"));
+        $this->assertNull($session->get("not_found"));
+        $this->assertEquals($session->get("one"), 1);
+    }
+
+    public function testWithValues()
+    {
+        $_SESSION["DRIPS_INFO"]["test"]["expire"] = true;
+        $_SESSION["DRIPS"]["test"] = "Test";
+        $_SESSION["DRIPS_INFO"]["test2"]["expire"] = -1;
+        $_SESSION["DRIPS"]["test2"] = "Test2";
+        $session = new Session();
+        $this->assertEquals($session->get("test"), "Test");
+        $this->assertEquals($session->get("test2"), "Test2");
+    }
+
+    public function testSerialize()
+    {
+        $session = new Session();
+        $obj = new StdClass;
+        $session->set("obj", $obj);
+        $this->assertEquals($session->get("obj"), $obj);
+    }
+
+    public function testStorage()
+    {
+        $session = new Session();
+        $result = $session->getAll();
+        $this->assertTrue(empty($result));
+        $session->set("abc", 123);
+        $result = $session->getAll();
+        $this->assertFalse(empty($result));
+    }
+
+    public function testWithExpire()
+    {
+        $session = new Session("MY_SESSION");
+        $session->set("key", 123, true);
+        $this->assertEquals($session->get("key"), 123);
+    }
+}

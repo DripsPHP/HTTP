9d702523e0066d10f2c93812c4eb9ef24ffe973f
Raffael Kessler <raffael.kessler@prowect.com>
2016-06-26 17:50:29 +0200

    session_start() nicht auf CLI

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
-if (session_status() != PHP_SESSION_ACTIVE && !headers_sent()) {
+if (session_status() != PHP_SESSION_ACTIVE && !headers_sent() && PHP_SAPI != 'cli') {
     session_start();
 }
